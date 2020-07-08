e5ff4020b383ac9b6f4ae8d10a699ee5854fb34a
Marco <marcotrumpet90@gmail.com>
2020-07-08 11:32:19 +0200

    Initial Commit

---
+++ onthemap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onthemap
+//
+//  Created by Marco Galetta on 08/07/2020.
+//  Copyright © 2020 Marco Galetta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
